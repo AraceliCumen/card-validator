71398f91b25a1d4224fd98eb43c621e3cc38a670
Araceli Cueva <aycuevam@gmail.com>
2018-01-29 00:03:10 -0500

    actualizando form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-let sum = (a, b) => console.log(a + b);
+// let sum = (a, b) => console.log(a + b);
 
-let pot = k => console.log(k * k);
+// let pot = k => console.log(k * k);
 
-pot(3);
+// pot(3);
 
-sum(1, 2);
-sum(2, 5);
+// sum(1, 2);
+// sum(2, 5);
+
+$(document).ready(() =>{
+   $cardNumber = $('#cn');
+   $expiryDate = $('#exp');
+   $cvv = $('#cvv');
+   $name = $('#name');
+   $buttonPay = $('#btn-pay');
+   
+   $formCardValidate = $('#form-dard-validate');
+   $
+
+
+});
